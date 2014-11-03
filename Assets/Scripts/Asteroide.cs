6425e2e4c6e959d347ed529a8bf887aebce6514b
bdaubord <baptistedaubord@gmail.com>
2014-11-03 12:07:30 +0100

    Update Asteroide.cs
    Modification de la vitesse de chaque asteroide (aléatoire);
    Rectification des max-min pour la génération des nombres aléatoire

--- Assets/Scripts/Asteroide.cs
+++ Assets/Scripts/Asteroide.cs
@@ -14,29 +14,31 @@ public class Asteroide : MonoBehaviour {
 		posy = 0;
 		choice = 0;
 	}
+	void size(){
+		int b = Random.Range (20, 150);
+		float bf = (float)(b * 0.01F);
+		transform.localScale = new Vector3 (bf, bf, bf);
+	}
 
 	void repop(){
 		choice = Random.Range (0, 2);
-		posy = Random.Range (-5, 5);
+		posy = Random.Range (-4, 8);
 		if (choice == 0) {
 			transform.position = new Vector3 (15, posy, 0);
 		}
 		else{
 			transform.position = new Vector3 (-15, posy, 0);
 		}
+		
+		size ();
+		
+	}
 
-		}
-	float size(){
-		int a = Random.Range (1, 3);
-		int b = Random.Range (0, 100);
-		float bf = (float)a + (float)(b * 0.01F);
-		transform.localScale = new Vector3 (bf, bf, bf);
-		return b;
-		}
 	void speedfunction(float neg){
+		speed = Random.Range(20, 100) * 0.001F;
 		transform.position -= new Vector3 (1*(neg) * this.speed, 0, 0);
 		transform.position += new Vector3 (0, coefdir * speed*(neg), 0);
-		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -44,46 +46,46 @@ public class Asteroide : MonoBehaviour {
 			//Re pop
 			repop ();
 			//Changement de la taille
-			float b = size ();
+			float b=0;
 			if(choice == 0){
-				if ((posy <= 5) && (posy >= 4))
-					b = Random.Range (-40, -25);
+				if ((posy <= 8) && (posy >= 5))
+					b = Random.Range (-40, 0);
+				if ((posy < 5) && (posy >= 4))
+					b = Random.Range (-30, 0);
 				if ((posy < 4) && (posy >= 3))
-					b = Random.Range (-30, -15);
-				if ((posy < 3) && (posy >= 2))
 					b = Random.Range (-20, 5);
-				if ((posy < 2) && (posy >= 1))
+				if ((posy < 3) && (posy >= 2))
 					b = Random.Range (-30, 15);
-				if ((posy < 1) && (posy >= -1))
+				if ((posy < 2) && (posy >= 1))
 					b = Random.Range (-30, 30);
-				if ((posy < -1) && (posy >= -2))
+				if ((posy < 1) && (posy >= -1))
 					b = Random.Range (-15, 30);
-				if ((posy < -2) && (posy >= -3))
+				if ((posy < -1) && (posy >= -2))
 					b = Random.Range (-5, 20);
-				if ((posy < -3) && (posy >= -4))
+				if ((posy < -2) && (posy >= -3))
 					b = Random.Range (15, 30);
-				if ((posy < -4) && (posy >= -5))
+				if ((posy < -3) && (posy >= -4))
 					b = Random.Range (25, 40);
 				coefdir = (float)b * 0.01F;
 			}
 			else{
-				if ((posy <= 5) && (posy >= 4))
-					b = Random.Range (40, 25);
+				if ((posy <= 8) && (posy >= 5))
+					b = Random.Range (40, 0);
+				if ((posy < 5) && (posy >= 4))
+					b = Random.Range (30, 0);
 				if ((posy < 4) && (posy >= 3))
-					b = Random.Range (30, 15);
-				if ((posy < 3) && (posy >= 2))
 					b = Random.Range (20, -5);
-				if ((posy < 2) && (posy >= 1))
+				if ((posy < 3) && (posy >= 2))
 					b = Random.Range (30, -15);
-				if ((posy < 1) && (posy >= -1))
+				if ((posy < 2) && (posy >= 1))
 					b = Random.Range (-30, 30);
-				if ((posy < -1) && (posy >= -2))
+				if ((posy < 1) && (posy >= -1))
 					b = Random.Range (15, -30);
-				if ((posy < -2) && (posy >= -3))
+				if ((posy < -1) && (posy >= -2))
 					b = Random.Range (5, -20);
-				if ((posy < -3) && (posy >= -4))
+				if ((posy < -2) && (posy >= -3))
 					b = Random.Range (-15, -30);
-				if ((posy < -4) && (posy >= -5))
+				if ((posy < -3) && (posy >= -4))
 					b = Random.Range (-25,- 40);
 				coefdir = (float)b * 0.01F;
 			}
